ff033245de3e5baa941b746a4d53ee0f589bfcd8
roberto-litroz <118381078+roberto-litroz@users.noreply.github.com>
2024-08-14 21:02:59 -0300

    Create product_test.go with tests for NewProduct and Validate functions. Necessary for validating product creation and input data.

---
+++ internal/entity/product_test.go
@@ -0,0 +1,41 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestNewProduct tests the NewProduct function
+func TestNewProduct(t *testing.T) {
+	product, err := NewProduct("Product 1", 10.5)
+	assert.Nil(t, err)
+	assert.NotNil(t, product)
+	assert.NotEmpty(t, product.ID)
+	assert.Equal(t, "Product 1", product.Name)
+	assert.Equal(t, 10.5, product.Price)
+}
+
+// Test when the product name is empty
+func TestProduct_Validate_EmptyName(t *testing.T) {
+	product, err := NewProduct("", 10.5)
+	assert.NotNil(t, err)
+	assert.Nil(t, product)
+	assert.Equal(t, ErrorNameRequired, err)
+}
+
+// Test when the product price is zero
+func TestProduct_Validate_ZeroPrice(t *testing.T) {
+	product, err := NewProduct("Product 1", 0)
+	assert.NotNil(t, err)
+	assert.Nil(t, product)
+	assert.Equal(t, ErrorPriceInvalid, err)
+}
+
+// Test when the product price is negative
+func TestProduct_Validate_NegativePrice(t *testing.T) {
+	product, err := NewProduct("Product 1", -10.5)
+	assert.NotNil(t, err)
+	assert.Nil(t, product)
+	assert.Equal(t, ErrorPriceInvalid, err)
+}
